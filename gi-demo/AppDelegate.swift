b45615c280d2847982fd13b34aa4d60e4671da06
Ben Scott <gamepoet@gmail.com>
2017-04-28 15:45:39 -0700

    Initial Commit

---
+++ gi-demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gi-demo
+//
+//  Created by Ben Scott on 4/28/17.
+//  Copyright © 2017 Ben Scott. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ gi-demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gi-demo
+//
+//  Created by Ben Scott on 4/28/17.
+//  Copyright © 2017 Ben Scott. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
